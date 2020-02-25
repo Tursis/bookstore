40f222cfa540352ed9dec2a80c1131a4b5bde57f
tursis <Tursis94@gmail.com>
2020-02-25 16:38:49 +0200

    new model book

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Book(models.Model):
+    name = models.TextField(max_length=100, help_text="Enter book name.")
+    author = models.TextField(max_length=50, help_text="Enter author name.")
+    pages = models.TextField(max_length=5, help_text="Enter number of pages")
