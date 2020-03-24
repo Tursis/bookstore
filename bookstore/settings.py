a0d3a489b2c855aacf21f2c3361e5a924fbd8149
Tursis <tursis94@gmail.com>
2020-03-24 16:41:21 +0200

    add two model BookAuthor and BookGenre.Add new  arguments blank and null in models. Add image fields in models

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- store/models.py
+++ store/models.py
@@ -3,33 +3,49 @@ from django.urls import reverse
 
 
 # Create your models here.
+class BookAuthor(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter author name.", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class BookGenre(models.Model):
+    genre = models.CharField(max_length=50, help_text="Enter book genre.", blank=True)
+
+    def __str__(self):
+        return self.genre
+
+
 class Book(models.Model):
-    name = models.CharField(max_length=100, help_text="Enter book name.")
-    author = models.CharField(max_length=50, help_text="Enter author name.")
-    pages = models.CharField(max_length=5, help_text="Enter number of pages")
-    publisher = models.CharField(max_length=50, help_text="Enter publisher name")
-    pub_year = models.IntegerField(help_text="Enter year of publication")
-    hard_cover = models.ImageField(upload_to='static/images/')
-    genre = models.CharField(max_length=100, help_text="Enter book genre.")
-    size = models.CharField(max_length=10, help_text="Enter size book")
-    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book")
-    Discounts = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter discounts")
+    name = models.CharField(max_length=100, help_text="Enter book name.", blank=True)
+    author = models.ManyToManyField(BookAuthor)
+    pages = models.CharField(max_length=5, help_text="Enter number of pages", blank=True)
+    publisher = models.CharField(max_length=50, help_text="Enter publisher name", blank=True)
+    pub_year = models.IntegerField(help_text="Enter year of publication", blank=True, null=True)
+    hard_cover = models.CharField(max_length=1, help_text="Enter hard cover book (+/-)", blank=True)
+    genre = models.ManyToManyField(BookGenre)
+    size = models.CharField(max_length=10, help_text="Enter size book", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book", blank=True)
+    Discounts = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter discounts", blank=True)
+    image = models.ImageField(upload_to='images/books', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Magazine(models.Model):
-    name = models.CharField(max_length=100, help_text="Enter name Magazine")
-    pages = models.IntegerField(help_text="Enter count pages Magazine")
-    publisher = models.CharField(max_length=50, help_text="Enter publisher name")
-    pub_year = models.IntegerField(help_text="Enter year of publication")
-    numb = models.IntegerField(help_text="Enter number Magazine")
-    numb_in_year = models.IntegerField(help_text="Enter number Magazine")
-    size = models.CharField(max_length=10, help_text="Enter size book")
-    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book")
-    subs_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book")
-    Discounts = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter discounts")
+    name = models.CharField(max_length=100, help_text="Enter name Magazine", blank=True)
+    pages = models.IntegerField(help_text="Enter count pages Magazine", blank=True, null=True)
+    publisher = models.CharField(max_length=50, help_text="Enter publisher name", blank=True)
+    pub_year = models.IntegerField(help_text="Enter year of publication", blank=True, null=True)
+    numb = models.IntegerField(help_text="Enter number Magazine", blank=True, null=True)
+    numb_in_year = models.IntegerField(help_text="Enter number Magazine", blank=True, null=True)
+    size = models.CharField(max_length=10, help_text="Enter size book", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book", blank=True, null=True)
+    subs_price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter price book", blank=True, null=True)
+    Discounts = models.DecimalField(max_digits=10, decimal_places=2, help_text="Enter discounts", blank=True, null=True)
+    image = models.ImageField(upload_to='images/magazine', blank=True, null=True)
 
     def __str__(self):
         return self.name
