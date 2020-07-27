66b9d90997d1c9cb29632d25803150e8b013fe55
tursis <Tursis94@gmail.com>
2020-07-27 16:57:05 +0300

    add registration template

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    path('store/', include('store.urls'))
+    path('store/', include('store.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
