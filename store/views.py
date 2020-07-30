779a76980949f0841273859429fd708462d0d92f
tursis <Tursis94@gmail.com>
2020-07-30 15:34:09 +0300

    change book_list and magazine_list, create tepmplate  goods_tile

--- store/views.py
+++ store/views.py
@@ -15,19 +15,19 @@ def index(request):
     book = Book.objects.all()
     magazine = Magazine.objects.all()
     genre = BookGenre.objects.all()
-    products = sorted(
+    products_list = sorted(
         chain(book, magazine),
         key=attrgetter('id'))
-    paginator = Paginator(products, 6)
+    paginator = Paginator(products_list, 6)
     page = request.GET.get('page')
     try:
-        products = paginator.page(page)
+        products_list = paginator.page(page)
     except PageNotAnInteger:
-        products = paginator.page(1)
+        products_list = paginator.page(1)
     except EmptyPage:
-        products = paginator.page(paginator.num_pages)
+        products_list = paginator.page(paginator.num_pages)
 
-    context = {'book': book, 'magazine': magazine, 'genre': genre, 'products': products}
+    context = {'book': book, 'magazine': magazine, 'genre': genre, 'products_list': products_list}
     return render(request, 'index.html', context=context)
 
 
@@ -47,13 +47,16 @@ def product_detail(request, slug):
 class ProductDetailView(generic.ListView):
     template_name = 'store/product_detail.html'
     context_object_name = "product"
+
     def get_queryset(self):
         return
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['books'] = Book.objects.all()
         context['magazine'] = Magazine.objects.all()
         context['product'] = sorted(chain(Book.objects.all(), Magazine.objects.all()), key=attrgetter('id'))
+        context['is_shown_by_default'] = True
         return context
 
 
@@ -64,12 +67,22 @@ class BookGenresListView(generic.ListView):
 
 class BooksListView(generic.ListView):
     model = Book
-    paginate_by = 3
+    paginate_by = 4
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Book.objects.all()
+        return context
 
 
 class MagazineListView(generic.ListView):
     model = Magazine
     paginate_by = 3
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Magazine.objects.all()
+        return context
 
 
 class BooksDetailView(generic.DetailView):
