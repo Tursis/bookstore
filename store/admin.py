ff53c46084a44e7def2426ec6f373c14888189ff
tursis <Tursis94@gmail.com>
2020-08-18 16:22:48 +0300

    add forms.py in accounts app

--- store/admin.py
+++ store/admin.py
@@ -6,6 +6,12 @@ from orders.models import Order, Purchase
 
 # Register your models here.
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'last_name', 'surname', 'birth_date')
+    list_filter = ['user']
+    search_fields = ['user']
+    radio_fields = {'gender': admin.VERTICAL}
+
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -24,10 +30,10 @@ class BookAuthorAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Magazine, MagazineAdmin)
 admin.site.register(BookGenre, BookGenreAdmin)
 admin.site.register(BookAuthor, BookAuthorAdmin)
-admin.site.register(Profile)
 admin.site.register(Order)
 admin.site.register(Purchase)
