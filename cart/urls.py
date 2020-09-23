10c6f4d2ba027186e3af279a3bda1c3f15964f1b
tursis <Tursis94@gmail.com>
2020-09-23 16:43:02 +0300

    delete  cart view and forms

--- cart/urls.py
+++ cart/urls.py
@@ -3,8 +3,4 @@ from django.urls import path
 from . import views
 
 app_name = 'cart'
-urlpatterns = [
-    path('', views.cart_detail, name='cart_detail'),
-    path('store/add/(<int:product_id>)/', views.cart_add, name='cart_add'),
-    path('store/remove/(<int:product_id>)/', views.cart_remove, name='cart_remove'),
-]
+urlpatterns = []
