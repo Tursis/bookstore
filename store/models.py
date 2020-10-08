a2c6616fac7a87d2e31942c47f3b26031c999025
tursis <Tursis94@gmail.com>
2020-10-08 10:48:06 +0300

    change models. Add new models: Product, Category, Publisher

--- store/models.py
+++ store/models.py
@@ -14,27 +14,40 @@ class BookAuthor(models.Model):
 
 
 class BookGenre(models.Model):
-    genre = models.CharField(max_length=50, verbose_name='Жанр', help_text="Enter book genre.", blank=True)
+    name = models.CharField(max_length=50, verbose_name='Жанр', help_text="Enter book genre.", blank=True)
     slug = models.SlugField(max_length=100)
 
     def __str__(self):
-        return self.genre
+        return self.name
 
     def get_absolute_url(self):
         return reverse('store:book_genres', kwargs={'slug': self.slug})
 
 
-class Book(models.Model):
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Категория товару', help_text="Enter product category.", blank=True)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Жанр', help_text="Enter publisher.", blank=True)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=100, verbose_name='Название', help_text="Enter book name.", blank=True)
-    author = models.ManyToManyField(BookAuthor, verbose_name='Автор')
     pages = models.CharField(max_length=5, verbose_name='Количество страниц', help_text="Enter number of pages",
                              blank=True)
-    publisher = models.CharField(max_length=50, verbose_name='Издатель', help_text="Enter publisher name", blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.SET_NULL, null=True)
     pub_year = models.IntegerField(verbose_name='Год издания', help_text="Enter year of publication", blank=True,
                                    null=True)
-    hard_cover = models.CharField(max_length=1, verbose_name='Твердая обложка', help_text="Enter hard cover book (+/-)",
-                                  blank=True)
-    genre = models.ManyToManyField(BookGenre, verbose_name='Жанр')
     size = models.CharField(max_length=10, verbose_name='Размеры', help_text="Enter size book", blank=True)
     price = models.DecimalField(max_digits=10, verbose_name='Цена', decimal_places=2, help_text="Enter price book",
                                 blank=True)
@@ -58,48 +71,24 @@ class Book(models.Model):
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
 
+
+class Book(Product):
+    author = models.ManyToManyField(BookAuthor, verbose_name='Автор')
+    hard_cover = models.CharField(max_length=1, verbose_name='Твердая обложка', help_text="Enter hard cover book (+/-)",
+                                  blank=True)
+    genre = models.ManyToManyField(BookGenre, verbose_name='Жанр')
+
     def get_absolute_url(self):
         return reverse('store:book_detail', kwargs={'slug': self.slug})
 
 
-class Magazine(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Название', help_text="Enter name Magazine", blank=True)
-    pages = models.IntegerField(verbose_name='Количество страниц', help_text="Enter count pages Magazine", blank=True,
-                                null=True)
-    publisher = models.CharField(max_length=50, verbose_name='Издатель', help_text="Enter publisher name", blank=True)
-    pub_year = models.IntegerField(verbose_name='Год издания', help_text="Enter year of publication", blank=True,
-                                   null=True)
+class Magazine(Product):
     numb = models.IntegerField(verbose_name='Номер журнала', help_text="Enter number Magazine", blank=True, null=True)
     numb_in_year = models.IntegerField(verbose_name='Номер журнала в году', help_text="Enter number Magazine",
                                        blank=True, null=True)
-    size = models.CharField(verbose_name='Размеры', max_length=10, help_text="Enter size book", blank=True)
-    price = models.DecimalField(max_digits=10, verbose_name='Цена', decimal_places=2, help_text="Enter price book",
-                                blank=True, null=True)
     subs_price = models.DecimalField(max_digits=10, verbose_name='Цена подписки', decimal_places=2,
                                      help_text="Enter price book", blank=True,
                                      null=True)
-    Discounts = models.DecimalField(max_digits=10, verbose_name='Скидка', decimal_places=2, help_text="Enter discounts",
-                                    blank=True, null=True)
-    image = models.ImageField(upload_to='images/magazine', verbose_name='Изображение', blank=True, null=True)
-    slug = models.SlugField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-    def __iter__(self):
-        for field in self._meta.fields:
-            yield (field.verbose_name)
-
-    class Meta:
-        ordering = ['-id']
-
-    def save(self, *args, **kwargs):  # new
-        if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('store:magazine_detail', kwargs={'slug': self.slug})
-
-    def get_group_permissions(self):
-        return set()
