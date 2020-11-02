843ce6ebdf36007eb9d2266d86321c2bd8c15137
tursis <Tursis94@gmail.com>
2020-11-02 16:53:51 +0200

    change cart file

--- cart/cart.py
+++ cart/cart.py
@@ -15,15 +15,15 @@ class CartInSession:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
+    def add(self, product_id, quantity=1, update_quantity=False):
         """
         Добавить продукт в корзину или обновить его количество.
         """
-        product_id = str(product.id)
+        product = Product.objects.get(id=product_id)
         if product_id not in self.cart:
-
-            self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
+            self.cart[product_id] = {'name': product,
+                                     'quantity': 0,
+                                     }
         if update_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -66,6 +66,7 @@ class CartInSession:
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
+
         """
         Подсчет стоимости товаров в корзине.
         """
