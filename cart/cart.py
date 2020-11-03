90d42acd6b1bee1679e7d79823285aec52da442e
tursis <Tursis94@gmail.com>
2020-11-03 16:26:11 +0200

    test cart

--- cart/cart.py
+++ cart/cart.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from store.models import Product
-from .cart_in_db import CartInDataBase
+from .cart_save import CartInDataBase, CartInSession
 
 
 class CartManager:
@@ -10,76 +10,22 @@ class CartManager:
         """
         Инициализируем корзину
         """
-        self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
-        if not cart:
-            cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart
+        if request.user.is_anonymous:
+            self.cart = CartInSession(request)
+
+        if request.user.is_authenticated:
+            self.cart = CartInDataBase()
 
     def add(self, request, product_id, quantity=1, update_quantity=False):
         """
         Добавить продукт в корзину или обновить его количество.
         """
         product = Product.objects.get(id=product_id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0,
-                                     'price': str(product.price)}
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        self.save()
+        if request.user.is_anonymous:
+            self.cart.add(product, quantity, update_quantity=False)
 
         if request.user.is_authenticated:
-            cart = CartInDataBase()
-            cart.add(request.user, product, quantity)
-
-    def save(self):
-        # Обновление сессии cart
-        self.session[settings.CART_SESSION_ID] = self.cart
-        # Отметить сеанс как "измененный", чтобы убедиться, что он сохранен
-        self.session.modified = True
-
-    def remove(self, product):
-        """
-        Удаление товара из корзины.
-        """
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
-    def __iter__(self):
-        """
-        Перебор элементов в корзине и получение продуктов из базы данных.
-        """
-        product_ids = self.cart.keys()
-        # получение объектов product и добавление их в корзину
-        products = Product.objects.filter(id__in=product_ids)
-        for product in products:
-            self.cart[str(product.id)]['product'] = product
-        for item in self.cart.values():
-            item['price'] = Decimal(item['price'])
-            item['total_price'] = item['price'] * item['quantity']
-            yield item
+            self.cart.add(request.user, product, quantity)
 
     def __len__(self):
-        """
-        Подсчет всех товаров в корзине.
-        """
-        return sum(item['quantity'] for item in self.cart.values())
-
-    def get_total_price(self):
-
-        """
-        Подсчет стоимости товаров в корзине.
-        """
-        return sum(Decimal(item['price']) * item['quantity'] for item in
-                   self.cart.values())
-
-    def clear(self):
-        """
-         удаление корзины из сессии
-        """
-        del self.session[settings.CART_SESSION_ID]
-        self.session.modified = True
+        return self.cart.__len__()
