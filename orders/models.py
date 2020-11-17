c14d4ec32ab514c30b6211315825dcd7c5eedff4
tursis <Tursis94@gmail.com>
2020-11-17 16:24:05 +0200

    update Order andPurchase models

--- orders/models.py
+++ orders/models.py
@@ -1,15 +1,29 @@
 from django.db import models
-from store.models import Book, Magazine
+from django.contrib.auth.models import User
+from store.models import Product, Book, Magazine
+from cart.models import Cart
+
+
 # Create your models here.
 
 class Order(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
-    magazine = models.ForeignKey(Magazine, on_delete=models.SET_NULL, null=True)
-    order_date = models.DateTimeField('order date')
+    first_name = models.CharField(max_length=50, verbose_name='Имя', null=True)
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия', null=True)
+    email = models.EmailField(verbose_name='Електронная почта', null=True)
+    address = models.CharField(max_length=250, verbose_name='Адрес', null=True)
+    postal_code = models.CharField(max_length=20, verbose_name='Почтовый код', null=True)
+    city = models.CharField(max_length=100, verbose_name='Город', null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False, verbose_name='Проплата', null=True)
 
 
 class Purchase(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    order_date = models.DateTimeField('order date')
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    quantity = models.PositiveIntegerField(default=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь', null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name='Заказ', null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Товар', null=True)
+    price = models.DecimalField(max_digits=10, verbose_name='Цена', decimal_places=2, help_text="Enter price book",
+                                blank=True)
+    quantity = models.IntegerField(verbose_name='Количество', help_text="Количество товара", blank=True, default=0,
+                                   null=True)
+    purchase_date = models.DateTimeField('Дата покупки', auto_now=True)
