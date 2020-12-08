e660a8bb6ae409cb14f536ca07a4c3c388425723
tursis <Tursis94@gmail.com>
2020-12-08 14:24:46 +0200

    add order from models cart

--- orders/forms.py
+++ orders/forms.py
@@ -1,8 +1,13 @@
 from django import forms
+from django.forms import TextInput
 from .models import Order
 
 
 class OrderCreateForm(forms.ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super(OrderCreateForm, self).__init__(*args, **kwargs)
+    #     self.fields['email'].initial = 'hello'
+
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.views.generic import View
 from cart.models import Cart
 from .models import Order, Purchase
@@ -10,10 +11,17 @@ from .orders import OrdersCreate
 class OrderView(View):
     def post(self, request):
         order_create = OrdersCreate(request)
+        user = User.objects.get(username=request.user)
         if request.method == 'POST':
             form = OrderCreateForm(request.POST)
             if form.is_valid():
                 order = form.save()
                 order_create.add_to_order(request, order)
                 return render(request, 'orders/created.html', {'order': order})
-        return render(request, 'orders/detail.html', {'form': OrderCreateForm})
+        if request.user.is_authenticated:
+            form = OrderCreateForm(initial={'first_name': user.first_name,
+                                            'last_name': user.last_name,
+                                            'email': user.email})
+        else:
+            form = OrderCreateForm
+        return render(request, 'orders/detail.html', {'form': form})
