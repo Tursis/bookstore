039556847be45d138481f96a7e7f5982ce77403c
tursis <Tursis94@gmail.com>
2020-12-17 16:30:31 +0200

    add order_detail

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,5 @@ app_name = 'order'
 urlpatterns = [
     path('', views.OrdersListView.as_view(), name='orders_list'),
     path('create/', views.OrderView.as_view(), name='order_view'),
+    path('order/<int:order_id>', views.OrdersDetailView.as_view(), name='order_detail'),
 ]
