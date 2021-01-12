00b4f0bc1875d6b930a770685868672b3941a0d5
Tursis <tursis94@gmail.com>
2021-01-12 23:02:58 +0200

    new js file

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,63 +1,63 @@
-var updateBtns = document.getElementsByClassName('update-cart')
+const total = document.querySelector("#card_form #total");
+const inputs = document.querySelectorAll("#card_form input");
 
-for (i = 0; i < updateBtns.length; i++) {
-    updateBtns[i].addEventListener('click', function () {
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
-        console.log('USER:', user)
-
-        if (user == 'AnonymousUser') {
-            addCookieItem(productId, action)
-        } else {
-            updateUserOrder(productId, action)
+totalUpdate();
+document.querySelectorAll("#card_form img").forEach((img) => {
+  img.addEventListener("click", (e) => {
+    const action = e.currentTarget.dataset.action;
+    const label = e.currentTarget.parentNode.parentNode;
+    const input = label.querySelector("input");
+    switch (action) {
+      case "card_form_plus":
+        input.value++;
+        send();
+        break;
+      case "card_form_minus":
+        if (Number(input.value) > 0) {
+          input.value--;
+          send();
         }
+        if (Number(input.value) === 0) {
+          removeElement(label);
+        }
+        break;
+      default:
+        break;
+    }
+    totalUpdate();
+  });
+});
+
+function send() {
+  const form = document.querySelector("#card_form");
+  const body = new FormData(form);
+  debug(body); // just for log, you can remove this line and function declaration
+  fetch("testing_list/", {
+    method: "POST",
+    body
+  })
+    .then((response) => {
+      // do something with response
     })
+    .catch((e) => {
+      // do something in case you got error
+    });
 }
 
-function updateUserOrder(productId, action) {
-    console.log('User is authenticated, sending data...')
-
-    var url = '/update_item/'
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-CSRFToken': csrftoken,
-        },
-        body: JSON.stringify({'productId': productId, 'action': action})
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            location.reload()
-        });
+function removeElement(element) {
+  element.parentNode.removeChild(element);
 }
 
-// function addCookieItem(productId, action) {
-//     console.log('User is not authenticated')
-//
-//     if (action == 'add') {
-//         if (cart[productId] == undefined) {
-//             cart[productId] = {'quantity': 1}
-//
-//         } else {
-//             cart[productId]['quantity'] += 1
-//         }
-//     }
-//
-//     if (action == 'remove') {
-//         cart[productId]['quantity'] -= 1
-//
-//         if (cart[productId]['quantity'] <= 0) {
-//             console.log('Item should be deleted')
-//             delete cart[productId];
-//         }
-//     }
-//     console.log('CART:', cart)
-//     document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
-//
-//     location.reload()
-// }
+function totalUpdate() {
+  let sum = 0;
+  inputs.forEach((input) => {
+    sum += input.value * input.dataset.price;
+  });
+  total.textContent = sum;
+}
+
+function debug(formData) {
+  for (const pair of formData.entries()) {
+    console.log(pair[0] + ", " + pair[1]);
+  }
+}
