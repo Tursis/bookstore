9a1fc64297e8fe93f587b8bd187a39708535dc81
tursis <Tursis94@gmail.com>
2021-01-14 16:36:13 +0200

    test CartUpdate post method

--- cart/urls.py
+++ cart/urls.py
@@ -6,7 +6,7 @@ app_name = 'cart'
 urlpatterns = [path('', views.cart_detail, name='cart_detail'),
                path('store/add/<int:product_id>/', views.CartAddView.as_view(), name='cart_add'),
                path('store/remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
-               path('testing_list/', views.testing_list, name="testing_list"),
+               path('cart_update/', views.CartUpdate.as_view(), name="cart_update"),
 
                ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- cart/views.py
+++ cart/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -43,72 +44,34 @@ def cart_detail(request):
     return render(request, 'cart/cart.html', {'cart': cart})
 
 
-def update_item(request):
-    data = json.loads(request.body)
-    productId = data['productId']
-    action = data['action']
-    print('Action:', action)
-    print('Product:', productId)
-
-    customer = request.user.customer
-    product = Product.objects.get(id=productId)
-
-    CartItem = Cart.objects.get(product=product)
-
-    if action == 'add':
-        CartItem.quantity = (CartItem.quantity + 1)
-    elif action == 'remove':
-        CartItem.quantity = (CartItem.quantity - 1)
-
-    CartItem.save()
-
-    if CartItem.quantity <= 0:
-        CartItem.delete()
-
-    return JsonResponse('Item was added', safe=False)
-
-
-@api_view(['GET', 'POST'])
-def testing_list(request, format=None):
+class CartUpdate(APIView):
     """
-    List all code snippets, or create a new snippet.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
-        snippets = Cart.objects.all()
-        serializer = CartSerializer(snippets, many=True)
+
+    def get(self, request, format=None):
+        cart = Cart.objects.all()
+        print('test')
+        serializer = CartSerializer(cart, many=True)
+        cart = Cart.objects.get(pk='116')
+        cart.quantity = (cart.quantity + 1)
+
+        cart.save()
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = CartSerializer(data=request.data)
-        if serializer.is_valid():
-            print('test')
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # data = serializer.data['product']
+        # print(data)
+        # CartItem = Cart.objects.get(product=data)
+        # CartItem.quantity = (CartItem.quantity + 1)
+        # CartItem.save()
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def testing_api(request, pk, format=None):
-    try:
-        cart = Cart.objects.get(pk=pk)
-    except Cart.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = CartSerializer(cart)
-        data = serializer.data['product']
-        print(data)
-        CartItem = Cart.objects.get(product=data)
-        CartItem.quantity = (CartItem.quantity + 1)
-        CartItem.save()
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = CartSerializer(cart, data=data)
-        if serializer.is_valid():
-            data = serializer.data['product']
-            print(data)
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+    def post(self, request, format=None):
+        cartM = CartManager(request)
+        cart = Cart.objects.all()
+        print('test')
+        serializer = CartSerializer(cart, many=True)
+        cart = Cart.objects.get(pk='116')
+        cart.quantity = (cart.quantity + 1)
+        cart.save()
+        cartM.get_total_price()
+        return Response('test')
