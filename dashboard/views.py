208b252c29d50f6030eabde9f6fffb6afae863d1
tursis <Tursis94@gmail.com>
2021-04-01 16:28:37 +0300

    add in template statistics ViewsStatistics

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,11 +4,13 @@ from django.views import View
 
 from bookstore.settings import PERMISSION_ON_SITE
 from .store_statistics import get_quantity_of_product_sold
-
+from .models import ViewStatistics
 
 class StatisticsView(PermissionRequiredMixin, View):
     permission_required = PERMISSION_ON_SITE['moderator']
 
     def get(self, request):
         return render(request, 'dashboard/statistics_detail.html',
-                      context={'get_quantity_of_product_sold': get_quantity_of_product_sold()})
+                      context={'get_quantity_of_product_sold': get_quantity_of_product_sold(),
+                               'get_views_product_statistics': ViewStatistics.objects.all()})
+
