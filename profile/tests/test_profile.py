276c2d09873dc45a9ba9b5fe2fddad593748e252
tursis <Tursis94@gmail.com>
2021-05-20 15:18:17 +0300

    add test change password and email

--- profile/tests/test_profile.py
+++ profile/tests/test_profile.py
@@ -1,33 +1,71 @@
-import urllib
+from django.core.exceptions import ValidationError
 from django.urls import reverse
 
 from django.contrib.auth.models import User
-from django.http import QueryDict
-from django.test import TestCase
-from django.utils.datastructures import MultiValueDict
+
+from django.test import RequestFactory, TestCase
 
 from profile.profile import change_password, change_profile_data, change_profile_email
+
+PROFILE_DETAIL_URL = reverse('profile:profile_detail')
 
 
 class MakingChangesProfileTest(TestCase):
     def setUp(self):
-        user = User.objects.create_user(username='Tursis')
-        user.set_password('123456')
-        user.email = 'oleh94@inbox.ru'
-        user.save()
+        self.factory = RequestFactory()
+        first_user = User.objects.create_user(username='Tursis')
+        first_user.set_password('123456')
+        first_user.email = 'oleh94@inbox.ru'
+        first_user.save()
+
+        second_user = User.objects.create_user(username='Milisento')
+        second_user.set_password('123456')
+        second_user.email = 'test@gmail.com'
+        second_user.save()
 
     def test_change_profile_data(self):
-        user = User.objects.get(pk=1)
-        test = {'csrfmiddlewaretoken': ['hXOApCs9SrjbvSPDIVzDbc6a1qUVVSmh4q5K0HrHVUuIUBtOpTtMvUwlZVcyu7Po'], 'last_name': ['Спиця'], 'name': ['Олег'], 'middle_name': ['Вікторович'], 'gender': ['М'],
-                   'birth_day': ['7'], 'birth_month': ['3'], 'birth_year': ['1994'], 'phone_number': [''],
-                   'email': ['tursis94@gmail.com'], 'old_password': [''],
-                   'new_password': [''], 'repeat_new_password': ['']}
 
-        login = self.client.login(username='Tursis', password='123456')
-
-        resp = self.client.post(reverse('profile:profile_detail'), data=test)
-        change_profile_data(resp.wsgi_request)
         user = User.objects.get(pk=1)
-        print(user.email)
-        print(resp.wsgi_request.POST)
-        self.assertEqual(user.email, 'hello')
+        data = {'last_name': 'Спиця', 'name': 'Олег', 'middle_name': 'Вікторович', 'gender': 'М',
+                'birth_day': '7', 'birth_month': '3', 'birth_year': '1994', 'phone_number': ''}
+        request = self.factory.post(PROFILE_DETAIL_URL, data=data)
+        request.user = user
+        change_profile_data(request)
+        user = User.objects.get(pk=1)
+
+        self.assertEqual(user.profile.birthday.day, int(data['birth_day']))
+        self.assertEqual(user.profile.birthday.month, int(data['birth_month']))
+        self.assertEqual(user.profile.birthday.year, int(data['birth_year']))
+        self.assertEqual(user.first_name, data['name'])
+        self.assertEqual(user.last_name, data['last_name'])
+        self.assertEqual(user.profile.surname, data['middle_name'])
+        self.assertEqual(user.profile.gender, data['gender'])
+
+    def test_change_profile_email(self):
+        user = User.objects.get(pk=1)
+        data = {'email': 'tursis94@gmail.com'}
+        request = self.factory.post(PROFILE_DETAIL_URL, data=data)
+        request.user = user
+        change_profile_email(request)
+        user = User.objects.get(pk=1)
+        self.assertEqual(user.email, data['email'])
+
+    def test_email_is_already_use(self):
+        user = User.objects.get(pk=1)
+        data = {'email': 'test@gmail.com'}
+        request = self.factory.post(PROFILE_DETAIL_URL, data=data)
+        request.user = user
+        with self.assertRaisesRegexp(ValidationError, 'email уже занят'):
+            change_profile_email(request)
+
+
+    def test_change_password(self):
+        user = User.objects.get(pk=1)
+        data = {'old_password': '123456', 'new_password': 'Qwe1432fQr', 'repeat_new_password': 'Qwe1432fQr'}
+        request = self.factory.post(PROFILE_DETAIL_URL, data=data)
+        request.user = user
+        change_password(request)
+        user = User.objects.get(pk=1)
+        self.assertTrue(user.check_password(data['new_password']))
+
+
