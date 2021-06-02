3dda4334402eb562cc01fd0d9a24d6c5316ffd99
tursis <Tursis94@gmail.com>
2021-06-02 13:56:06 +0300

    add test_unit_core

--- comments/tests/test_views.py
+++ comments/tests/test_views.py
@@ -1,8 +1,4 @@
-from io import BytesIO
 from unittest import mock
-
-from PIL import Image
-from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from django.test import TestCase
 
@@ -11,7 +7,8 @@ from django.test.client import Client, RequestFactory
 from django.urls import reverse
 
 from comments.forms import ProductReviewsForm
-from store.models import Book, Magazine, Product, Category, Publisher, BookAuthor, BookGenre
+from store.models import Product
+from test_unit_core.test_core import create_product_for_test
 
 
 class ProductReviewsTest(TestCase):
@@ -23,32 +20,7 @@ class ProductReviewsTest(TestCase):
         user.email = 'test1@gmail.com'
         user.save()
         login = self.client.login(username='Tursis', password='123456')
-
-        number = 2
-        [Category.objects.create(name=category_item) for category_item in ('Books', 'Magazine')]
-        [Publisher.objects.create(name=publisher_item) for publisher_item in range(1, number)]
-        [BookAuthor.objects.create(name=author_item) for author_item in range(1, number)]
-        [BookGenre.objects.create(name=genre_item) for genre_item in range(1, number)]
-
-        image = Image.new('RGBA', size=(50, 50), color=(256, 0, 0))
-        image_file = BytesIO(image.tobytes())
-        file = InMemoryUploadedFile(image_file, None, 'test.jpg', 'image/jpg', 1024, None)
-
-        for product_item in range(1, number):
-            Book.objects.create(category=Category.objects.get(pk=1), name='book_%s' % product_item,
-                                publisher=Publisher.objects.get(pk=product_item),
-                                price=product_item * 10, Discounts=0, hard_cover='+', pages=5 * product_item,
-                                pub_year=2000 + product_item, size='240x165', image=file
-                                )
-
-            Book.objects.get(pk=product_item).author.add(BookAuthor.objects.get(pk=product_item))
-            Book.objects.get(pk=product_item).genre.add(BookGenre.objects.get(pk=product_item))
-        for product_item in range(1, number):
-            Magazine.objects.create(category=Category.objects.get(pk=2), name='magazine_%s' % product_item,
-                                    publisher=Publisher.objects.get(pk=product_item),
-                                    price=product_item * 10, Discounts=0, numb=product_item, numb_in_year=product_item,
-                                    subs_price=20 * product_item, pages=5 * product_item,
-                                    pub_year=2000 + product_item, size='240x165', image=file)
+        create_product_for_test(2)
 
     def test_product_reviews_url(self):
         product = Product.objects.get(pk=1)
--- store/tests/test_views.py
+++ store/tests/test_views.py
@@ -1,36 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from store.models import Product, Book, Magazine, Category, Publisher, BookAuthor, BookGenre
 from django.urls import reverse
+
+from test_unit_core.test_core import create_product_for_test
 
 
 class ProductListViewTest(TestCase):
 
     @classmethod
     def SetUpTestData(cls):
-        number = 13
-        [Category.objects.create(name=category_item) for category_item in range(1, 3)]
-        [Publisher.objects.create(name=publisher_item) for publisher_item in range(1, number)]
-        [BookAuthor.objects.create(name=author_item) for author_item in range(1, number)]
-        [BookGenre.objects.create(name=genre_item) for genre_item in range(1, number)]
-
-        for product_item in range(1, number):
-            Book.objects.create(category=Category.objects.get(pk=1), name='book_%s' % product_item,
-                                publisher=Publisher.objects.get(pk=product_item),
-                                price=product_item * 10, Discounts=0, hard_cover='+', pages=5 * product_item,
-                                pub_year=2000 + product_item, size='240x165'
-                                )
-
-            Book.objects.get(pk=product_item).author.add(BookAuthor.objects.get(pk=product_item))
-            Book.objects.get(pk=product_item).genre.add(BookGenre.objects.get(pk=product_item))
-
-        for product_item in range(1, number):
-            Magazine.objects.create(category=Category.objects.get(pk=2), name='magazine_%s' % product_item,
-                                    publisher=Publisher.objects.get(pk=product_item),
-                                    price=product_item * 10, Discounts=0, numb=product_item, numb_in_year=product_item,
-                                    subs_price=20 * product_item, pages=5 * product_item,
-                                    pub_year=2000 + product_item, size='240x165')
+        create_product_for_test(2)
 
     def test_get_response(self):
         resp = self.client.get('index.html')
