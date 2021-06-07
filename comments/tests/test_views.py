31a7d714293d7994fb42aeef91c2a305b3187854
tursis <Tursis94@gmail.com>
2021-06-07 14:01:41 +0300

    add test review comemnt

--- comments/tests/test_views.py
+++ comments/tests/test_views.py
@@ -2,12 +2,11 @@ from unittest import mock
 
 from django.test import TestCase
 
-
 from django.test.client import Client, RequestFactory
 from django.urls import reverse
 
 from comments.forms import ProductReviewsForm
-from store.models import Product
+from store.models import Product, Book
 from test_unit_core.test_core import create_product_for_test, create_user
 
 
@@ -52,4 +51,36 @@ class ProductReviewsTest(TestCase):
         self.client.logout()
         product = Product.objects.get(pk=1)
         resp = self.client.post(reverse('reviews:product_reviews', args=(product.slug,)))
+        self.assertEqual(resp.status_code, 302)
+
+
+class ReviewCommentTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        create_user('Tursis', '123456', 'test@gmail.com')
+        create_product_for_test(2)
+        login = self.client.login(username='Tursis', password='123456')
+
+    def tearDown(self):
+        for i in Product.objects.all():
+            i.image.delete()
+
+    def test_book_detail_url(self):
+        product = Book.objects.get(pk=1)
+        resp = self.client.post(reverse('store:book_detail', args=(product.slug,)), follow=True)
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'store/book/book_detail.html', product.slug)
+
+    @mock.patch('comments.views.add_review_comment')
+    def test_called_function_add_review_comment(self, mock_add_review_comment):
+        product = Product.objects.get(pk=1)
+        form_data = {'comment': 'Nice'}
+        resp = self.client.post(reverse('store:book_detail', args=(product.slug,)), form_data)
+        mock_add_review_comment.assert_called()
+
+    def test_access_to_add_review_comment_if_user_no_login(self):
+        self.client.logout()
+        product = Product.objects.get(pk=1)
+        form_data = {'comment': 'Nice'}
+        resp = self.client.post(reverse('store:book_detail', args=(product.slug,)), form_data)
         self.assertEqual(resp.status_code, 302)
