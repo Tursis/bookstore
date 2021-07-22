fd468612016698fc27640e5e7248909fe4ec5091
tursis <Tursis94@gmail.com>
2021-07-22 11:55:56 +0300

    add test call add_to_order

--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -13,6 +13,13 @@ from store.models import Product
 from test_unit_core.test_core import create_user, create_product_for_test
 
 
+def order_create_form_for_test():
+    form_data = {'first_name': 'name', 'last_name': 'last_name', 'email': 'test@gmail.com', 'address': 'test 10',
+                 'postal_code': '1234', 'city': 'Test City'}
+    form = OrderCreateForm(form_data)
+    return form
+
+
 class OrderViewTest(TestCase):
 
     def setUp(self):
@@ -30,7 +37,13 @@ class OrderViewTest(TestCase):
         self.assertTemplateUsed(resp, 'orders/create.html')
 
     def test_form_valid(self):
-        form_data = {'first_name': 'name', 'last_name': 'last_name', 'email': 'test@gmail.com', 'address':'test 10', 'postal_code':'1234', 'city':'Test City'}
-        form = OrderCreateForm(form_data)
+        form = order_create_form_for_test()
         resp = self.client.post(reverse('order:order_view'))
         self.assertTrue(form.is_valid())
+
+    @mock.patch('orders.views.OrdersCreate.add_to_order')
+    def test_call_method_add_to_order(self, mock_add_to_order):
+        form = order_create_form_for_test()
+        print(form.data)
+        resp = self.client.post(reverse('order:order_view'), form.data)
+        mock_add_to_order.assert_called()
